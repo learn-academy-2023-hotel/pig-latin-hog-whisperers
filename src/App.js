853af23d09e3d10c4ab528aeff53aecf5595ef56
jonathanraya <jonathanraya79@gmail.com>
2023-11-22 21:45:57 -0600

    Changed styling on webpage

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import "./App.css"
 import butcherPigImage from "./assets/butcherPig.jpeg"
 
 const App = () => {
-  // ACTION ITEM: to make the development process easier there are some preassigned words in the input field, when you are ready for your full user experience delete the test words passed to useState and pass an empty string
+  
   const [userInput, setUserInput] = useState(
-    "apple through queen squeal fry fluent"
+    ""
   )
   const [inputTranslated, setInputTranslated] = useState("")
 
-  // ACTION ITEM: the "myPigLatinCodeHere" function is where you will put your logic to translate the sentence entered by the user into Pig Latin
   const myPigLatinCodeHere = () => {
   
-    // // NO MODIFICATION NEEDED: the variable "arrayOfUserInput" will contain the text input from the user split into an array of words
     const arrayOfUserInput = userInput.split(" ")
     console.log("arrayOfUserInput:", arrayOfUserInput)
 
-    // NO MODIFICATION NEEDED: now that we have an array of words, we can map over the array and look at each word
     const translatedWordsArray = arrayOfUserInput.map((eachWord) => {
       console.log("eachWord:", eachWord)
 
-      // NO MODIFICATION NEEDED: this code will look at each word and identify the vowels
+
       const vowelsArray = eachWord.split("").filter((vowel) => {
         return (
           vowel === "a" ||
@@ -32,63 +29,43 @@ const App = () => {
       })
       console.log("vowelsArray:", vowelsArray)
 
-      // ACTION ITEM: your Pig Latin logic goes here!
-     
-      
-
-      if (eachWord.charAt(0) === "a" ||
-      eachWord.charAt(0) === "e" ||
-      eachWord.charAt(0) === "i" ||
-      eachWord.charAt(0) === "o" ||
-      eachWord.charAt(0) === "u") {
+      //  Pig Latin logic 
+      if (eachWord.charAt(0).includes(vowelsArray[0])) {
         return eachWord + "way"
       } else if (eachWord.charAt(0) === "q") {
         return eachWord.slice(2) + "quway"
       } else if (eachWord.includes("squ")) {
         return eachWord.slice(3) + "squway"
       } else if (eachWord.charAt(eachWord.length - 1) === "y" &&
-      !eachWord.includes("a") &&
-      !eachWord.includes("e") &&
-      !eachWord.includes("i") &&
-      !eachWord.includes("o") &&
-      !eachWord.includes("u")) {
+      eachWord.includes(vowelsArray)) {
         return eachWord.slice(eachWord.length - 1) + eachWord.slice(0, -1) + "ay"
-      } else if (eachWord.charAt(0) !== "a" &&
-      eachWord.charAt(0) !== "e" &&
-      eachWord.charAt(0) !== "i" &&
-      eachWord.charAt(0) !== "o" &&
-      eachWord.charAt(0) !== "u") {
-        return eachWord.slice(eachWord.indexOf(vowelsArray))
-      }
-      else {
+      } else if (eachWord.indexOf(vowelsArray) !== 0) {
+        return eachWord.slice(eachWord.indexOf(vowelsArray[0])) + eachWord.slice(0, eachWord.indexOf(vowelsArray[0])) + "ay"
+      } else {
         return "oink"
       }
-
-      // ACTION ITEM: this return will be the output of your Pig Latin'd code
-      // return eachWord
     })
 
-    // NO MODIFICATION NEEDED: once the code has been modified it gets joined from an array back to a string
+    //once the code has been modified it gets joined from an array back to a string
     const translatedWords = translatedWordsArray.join(" ")
     console.log("translatedWords:", translatedWords)
 
-    // NO MODIFICATION NEEDED: this will update the inputTranslated variable in state
+    //  this will update the inputTranslated variable in state
     setInputTranslated(translatedWords)
   }
 
-  // ACTION ITEM: this method restarts the game by setting the original state, when you are ready for your full user experience delete the test words in setUserInput and pass an empty string
   const restartGame = () => {
-    setUserInput("apple through queen squeal fry fluent")
+    setUserInput("")
     setInputTranslated("")
   }
 
-  // NO MODIFICATION NEEDED: this method prevents React from refreshing the page unnecessarily
+  //  prevents React from refreshing the page unnecessarily
   const setUpPreventDefault = (e) => {
     e.preventDefault()
     myPigLatinCodeHere()
   }
 
-  // NO MODIFICATION NEEDED: this method takes the value of the input and saves it in state
+  //takes the value of the input and saves it in state
   const handleInput = (e) => {
     setUserInput(e.target.value)
   }
@@ -97,14 +74,8 @@ const App = () => {
     <div className="page-container">
       <div className="body-container">
         <h1>Pig Latin Translator</h1>
-        <img
-          src={butcherPigImage}
-          alt="pig with butcher cut names in pig latin"
-          className="butcher-pig-image"
-        />
-
         <div className="input-section">
-          <h4>Enter phrase to be translated:</h4>
+          <h4>Type in words to translate them to Pig Latin</h4>
           <input
             type="text"
             className="user-input"
@@ -117,7 +88,7 @@ const App = () => {
         </div>
         <p>{inputTranslated}</p>
       </div>
-      <footer>&copy; 2023 | Coded by: Your Names Here!</footer>
+      <footer>&copy; 2023 | Jonathan Raya</footer>
     </div>
   )
 }
